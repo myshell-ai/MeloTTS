99934455a14c8e9d0596ca21650dda09beb4e9b2
Cihan Yalçın <113353248+g-hano@users.noreply.github.com>
2024-12-23 15:25:49 +0300

    Update cleaner.py

--- melo/text/cleaner.py
+++ melo/text/cleaner.py
@@ -1,9 +1,9 @@
-from . import chinese, japanese, english, chinese_mix, korean, french, spanish
+from . import chinese, japanese, english, chinese_mix, korean, french, spanish, turkish
 from . import cleaned_text_to_sequence
 import copy
 
 language_module_map = {"ZH": chinese, "JP": japanese, "EN": english, 'ZH_MIX_EN': chinese_mix, 'KR': korean,
-                    'FR': french, 'SP': spanish, 'ES': spanish}
+                    'FR': french, 'SP': spanish, 'ES': spanish, "TR": turkish}
 
 
 def clean_text(text, language):
